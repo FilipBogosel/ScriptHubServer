80b8a8a2386d4df18761157d28f3dcb640c44b5e
FilipBogosel <101122946+FilipBogosel@users.noreply.github.com>
2025-09-04 22:28:11 +0300

    Refactor user schema to remove uniqueness constraint on providerId and add composite index for provider and providerId

--- models/User.js
+++ models/User.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose"
 // Schema for users in the database, to ensure data integrity and consistency
 const userSchema = new mongoose.Schema({
     "provider":{ type: String, required: true, trim: true, enum: ['apple', 'google', 'github', 'facebook', 'linkedin'], default: 'google' },
-    "providerId": { type: String, required: true, unique: true, trim: true },
+    "providerId": { type: String, required: true, trim: true },
     "username": { type: String, required: true, unique: true, minLength: 3, maxLength: 30, trim: true },
     "email": { type: String, required: true, unique: true, trim: true },
 });
+userSchema.index({ provider: 1, providerId: 1 }, { unique: true });
 
 export default mongoose.model('User', userSchema);
