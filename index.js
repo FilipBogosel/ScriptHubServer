cb6aa5ccded78239a8e60801c5c8563ff98bc9ef
FilipBogosel <101122946+FilipBogosel@users.noreply.github.com>
2025-09-15 13:47:29 +0300

    lax cookie

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24,
         secure: false, // Set to false for Electron compatibility
         httpOnly: true,
-        sameSite: 'none' // Use 'lax' instead of conditional 'none'
+        sameSite: 'lax' // Use 'lax' instead of conditional 'none'
     }
 }));
 app.use(passport.initialize());
